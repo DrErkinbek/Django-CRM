bc058b619120341db128b82a287cf1d92f7e6ba8
Erkinbek <erkinbekabdurakhmanov@gmail.com>
2026-01-02 21:17:30 -0600

    delete records

--- website/views.py
+++ website/views.py
@@ -47,3 +47,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have logged out.')
     return redirect('home')
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look up record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be Logged In to view the page...")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be Logged In to view the page...")
+        return redirect('home')
